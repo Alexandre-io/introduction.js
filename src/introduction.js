f65a5ad16b4aa5aca021507abb9dfd67d4d144a4
Alexandre-io <alexandre@gotoserv.com>
2015-12-09 19:45:57 +0100

    fix(codacy): codacy recommandations

--- src/introduction.js
+++ src/introduction.js
@@ -502,7 +502,7 @@
     tooltipLayer.className = ('introjs-tooltip ' + tooltipCssClass).replace(/^\s+|\s+$/g, '');
 
     currentTooltipPosition = this._introItems[this._currentStep].position;
-    if ((currentTooltipPosition === "auto" || this._options.tooltipPosition == "auto")) {
+    if ((currentTooltipPosition === "auto" || this._options.tooltipPosition === "auto")) {
       if (currentTooltipPosition !== "floating") { // Floating is always valid, no point in calculating
         currentTooltipPosition = _determineAutoPosition.call(this, targetElement, tooltipLayer, currentTooltipPosition);
       }
